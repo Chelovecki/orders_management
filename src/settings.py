03aa308e6f374bae424821ea243b496614efe756
chelovecki <benkozlovv@gmail.com>
2026-02-10 12:45:15 +0300

    add: settings for conn postgres

--- src/settings.py
+++ src/settings.py
@@ -5,10 +5,21 @@ load_dotenv('docker/.env')
 
 
 class PostgresSettings:
-    POSTGRES_DB = os.getenv('POSTGRES_DB', )
-    POSTGRES_USERNAME = os.getenv('POSTGRES_USERNAME')
-    POSTGRES_PORT = int(os.getenv('POSTGRES_PORT', '5432'))
-    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+    HOST = os.getenv('POSTGRES_HOST')
+    DB = os.getenv('POSTGRES_DB')
+    USERNAME = os.getenv('POSTGRES_USERNAME')
+    PORT = int(os.getenv('POSTGRES_PORT', '5432'))
+    PASSWORD = os.getenv('POSTGRES_PASSWORD')
 
-    if not POSTGRES_PASSWORD:
+    if not PASSWORD:
         raise Exception('The password is required. Set it in ".env"')
+
+    @classmethod
+    def get_async_url(cls):
+        return f"postgresql+asyncpg://{cls.USERNAME}:{cls.PASSWORD}@{cls.HOST}:{cls.PORT}/{cls.DB}"
+
+    @classmethod
+    def get_sync_url(cls):
+        return f"postgresql+psycopg2://{cls.USERNAME}:{cls.PASSWORD}@{cls.HOST}:{cls.PORT}/{cls.DB}"
+
+
